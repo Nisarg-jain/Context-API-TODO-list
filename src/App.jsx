1db3dffd5e6612ea0251f4adc6a20bccd04fc02f
Nisarg-jain <jainnisag98@gmail.com>
2026-06-12 19:33:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-6xl font-bold underline bg-purple-900 text-white">hello react</h1>
+  )
+}
+
+
+export default App
